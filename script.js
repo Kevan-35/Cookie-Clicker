b3e6e97bfad2695d0a07fefcf7f0dc7361bb0470
Kevan <poirierkevan@gmail.com>
2024-10-10 09:49:44 +0200

    fonctionnalités de base marchent, multiplicateur / autoclicker

--- script.js
+++ script.js
@@ -2,19 +2,28 @@
         constructor() {
             this.total_score = 0 //achievement
             this.score = 0
-            this.i = 1
+            this.one_click = 1
+
             this.prix_multiplicateur_2_clique = 50
+            this.prix_autoclicker = 20
+
+        }
+        update_buy_button(buyButton) {
+            buyButton.textContent = "Acheter : " + this.prix_multiplicateur_2_clique; // Met à jour le texte du bouton
+        }
+        update_autoclicker_button(autoclickerButton) {
+            autoclickerButton.textContent = "Acheter : " + this.prix_autoclicker; // Met à jour le texte du bouton
         }
         increment_score() {
-            this.score += this.i
-            this.total_score += this.i
+            this.score += this.one_click
+            this.total_score += this.one_click
         }
         click(){
             this.increment_score()
 
         }
         multiplicateur_2_clique(){
-            this.i *= Math.ceil(1.5)
+            this.one_click *= Math.ceil(1.5)
             
         }
         buy(){
@@ -24,11 +33,19 @@
                 this.multiplicateur_2_clique()
             }
         }
-        update_buy_button(buyButton) {
-            buyButton.textContent = "Acheter : " + this.prix_multiplicateur_2_clique; // Met à jour le texte du bouton
+        timer(){
+            setInterval(() => {
+                this.score += 1
+                score.textContent = "Score : " + game.score;
+
+                console.log(this.score)
+            }, 1000);
         }
-        autoclick(){
-            
+        autoclicker(){
+            if (this.score >= this.prix_autoclicker){
+                this.score -= this.prix_autoclicker
+                this.timer()
+            }
         }
     }
 
@@ -37,19 +54,28 @@
     clique = document.getElementById("clique")
     score = document.getElementById("score")
     buy = document.getElementById("buy")
+    autoclicker = document.getElementById("autoclicker")
+
 
     game.update_buy_button(buy)
+    game.update_autoclicker_button(autoclicker)
+
     
     clique.addEventListener("click", function() {
         game.click()
         score.textContent = "Score : " + game.score;
     }); 
-
     
     buy.addEventListener("click", function() {
         game.buy()  
         score.textContent = "Score : " + game.score;
         buy.textContent = "Acheter : " + game.prix_multiplicateur_2_clique;
+    });
+
+    autoclicker.addEventListener("click", function() {
+        game.autoclicker()  
+        score.textContent = "Score : " + game.score;
+        autoclicker.textContent = "Acheter : " + game.prix_autoclicker;
     });
 
 
