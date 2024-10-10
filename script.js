96422f1e22b62d1011e25a53965ed58cd61bb376
Kevan <poirierkevan@gmail.com>
2024-10-10 17:09:48 +0200

    ajout de la fonctionnalité score d'attente
    - Ajout du système de score d'attente qui stocke les points dans une variable temporaire avant de les transférer au score total.
    - Mise en place d'une limite maximum pour le score d'attente (max_score).
    - Le score d'attente est transféré automatiquement lorsque la limite est atteinte.
    - Affichage du score d'attente mis à jour dans l'interface utilisateur.

--- script.js
+++ script.js
@@ -2,41 +2,44 @@
         constructor() {
             this.total_score = 0 //achievement
             this.score = 0
+            this.score_attente = 0
             this.one_click = 1
+            this.max_score = 20
 
-            this.prix_multiplicateur_2_clique = 50
-            this.prix_autoclicker = 20
+            this.prix_acheter_bonus_clique = 18
+            this.prix_autoclicker = 15
 
         }
-        update_buy_button(buyButton) {
-            buyButton.textContent = "Acheter : " + this.prix_multiplicateur_2_clique; // Met à jour le texte du bouton
+        update_acheter_bonus_clique_button(acheter_bonus_cliqueButton) {
+            acheter_bonus_cliqueButton.textContent = "Acheter le multiplicateur : " + this.prix_acheter_bonus_clique; // Met à jour le texte du bouton
         }
         update_autoclicker_button(autoclickerButton) {
-            autoclickerButton.textContent = "Acheter : " + this.prix_autoclicker; // Met à jour le texte du bouton
+            autoclickerButton.textContent = "Acheter l'autoclicker : " + this.prix_autoclicker; // Met à jour le texte du bouton
         }
         increment_score() {
-            this.score += this.one_click
+            this.score_attente += this.one_click
             this.total_score += this.one_click
+            this.max_score_attente()
         }
         click(){
             this.increment_score()
 
         }
-        multiplicateur_2_clique(){
-            this.one_click *= Math.ceil(1.5)
-            
+        bonus_clique(){
+            this.one_click += Math.ceil(1)
         }
-        buy(){
-            if (this.score >= this.prix_multiplicateur_2_clique) {
-                this.score -= this.prix_multiplicateur_2_clique
-                this.prix_multiplicateur_2_clique = Math.ceil(this.prix_multiplicateur_2_clique * 1.15)
-                this.multiplicateur_2_clique()
+        acheter_bonus_clique(){
+            if (this.score >= this.prix_acheter_bonus_clique) {
+                this.score -= this.prix_acheter_bonus_clique
+                this.prix_acheter_bonus_clique = Math.ceil(this.prix_acheter_bonus_clique * 1.15)
+                this.bonus_clique()
             }
         }
         timer(){
             setInterval(() => {
-                this.score += 1
-                score.textContent = "Score : " + game.score;
+                this.score_attente += 1
+                this.max_score_attente()
+                score_attente.textContent = "Score d'attente : " + this.score_attente;
 
                 console.log(this.score)
             }, 1000);
@@ -45,40 +48,56 @@
             if (this.score >= this.prix_autoclicker){
                 this.score -= this.prix_autoclicker
                 this.timer()
+                console.log(this.score_attente)
             }
         }
+        transport_score(){
+            if (this.score_attente = this.max_score){
+                this.score += this.score_attente
+                this.score_attente = 0
+                score.textContent = "Score : " + this.score;
+            }
+        }
+        max_score_attente(){
+            if (this.score_attente >= this.max_score){
+                this.score_attente = this.max_score
+                this.transport_score()
+            }
+        }
+        
     }
 
 
     const game = new Game();
     clique = document.getElementById("clique")
     score = document.getElementById("score")
-    buy = document.getElementById("buy")
+    score_attente = document.getElementById("score_attente")
+    acheter_bonus_clique = document.getElementById("acheter_bonus_clique")
     autoclicker = document.getElementById("autoclicker")
 
 
-    game.update_buy_button(buy)
+    game.update_acheter_bonus_clique_button(acheter_bonus_clique)
     game.update_autoclicker_button(autoclicker)
 
     
     clique.addEventListener("click", function() {
         game.click()
-        score.textContent = "Score : " + game.score;
+        score_attente.textContent = "Score d'attente : " + game.score_attente;
     }); 
     
-    buy.addEventListener("click", function() {
-        game.buy()  
+    acheter_bonus_clique.addEventListener("click", function() {
+        game.acheter_bonus_clique()  
         score.textContent = "Score : " + game.score;
-        buy.textContent = "Acheter : " + game.prix_multiplicateur_2_clique;
+        acheter_bonus_clique.textContent = "Acheter le multiplicateur : " + game.prix_acheter_bonus_clique;
     });
 
     autoclicker.addEventListener("click", function() {
-        game.autoclicker()  
+        game.autoclicker()
         score.textContent = "Score : " + game.score;
-        autoclicker.textContent = "Acheter : " + game.prix_autoclicker;
+        score_attente.textContent = "Score d'attente : " + game.score_attente;
+        autoclicker.textContent = "Acheter l'autoclicker : " + game.prix_autoclicker;
     });
-
-
+    
 
 
             
