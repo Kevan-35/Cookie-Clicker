1d37d849724c9a9a247c1894578902d248c5eccd
Kevan <poirierkevan@gmail.com>
2024-10-21 16:14:59 +0200

    nouvelles fonctionnalités : différents états pour les étages : - état qui rend l'étage accessible, le minerai cliquable... - état où quand le score de l'utilisateur est supérieur ou égal au prix de l'étage, un bouton apparait avec la possibilité d'acheter l'étage. -sinon l'étage est inaccessible, incliquable, avec l'ajout d'un cadena montrant que l'étage ne peut etre utilisé

--- Etage.js
+++ Etage.js
@@ -1,10 +1,11 @@
 class Etage{
-	constructor(nom_minerai, image_minerai, prix_minerai = 0, nb_click = 1, nb_autoclick = 1){
+	constructor(nom_minerai, image_minerai, nb_click = 1, prix_etage = 0, nb_autoclick = 1, est_achete = false){
 		this.nom_minerai = nom_minerai
-		this.image_minerai= image_minerai
-		this.prix_minerai = prix_minerai 
+		this.image_minerai = image_minerai
 		this.nb_click = nb_click 
+		this.prix_etage = prix_etage 
 		this.nb_autoclick= nb_autoclick
+		this.est_achete = est_achete
 	}
 }
 
--- script.js
+++ script.js
@@ -6,52 +6,104 @@
             this.one_click = 1  
 
             // count
-            this.nb_bonus_clique = 0
+            this.nb_bonus_click = 0
             this.nb_autoclicker = 0 
 
             // prix
-            this.prix_acheter_bonus_clique = 18
+            this.prix_acheter_bonus_click = 18
             this.prix_autoclicker = 15
 
-            this.update_acheter_bonus_clique_button()
+            this.update_acheter_bonus_click_button()
             this.update_autoclicker_button()
 
             this.etages = []
             this.initEtage()
             this.displayEtages()
 
+
         }
         initEtage(){
-            this.etages.push(new Etage("Pierre", "images/pierre.png"))
-            this.etages.push(new Etage("Or", "images/or.png"))
-            this.etages.push(new Etage("Diament", "images/diament.png"))
-            this.etages.push(new Etage("Emeraude", "images/emeraude.png"))
+            this.etages.push(new Etage("Pierre", "images/pierre.png", 1, 10, 100, true))
+            this.etages.push(new Etage("Or", "images/or.png", 100, 10, 10, false))
+            this.etages.push(new Etage("Diament", "images/diament.png", 100, 100, 10, false))
+            this.etages.push(new Etage("Emeraude", "images/emeraude.png", 1000, 1000, 10, false));
 
-
-            
-           
-            
         }
-        displayEtages(){
-            let parent = document.querySelector('#middle-section')
+        displayEtages() {
+            let parent = document.querySelector('#middle-section');
+            parent.innerHTML = ''; 
+        
             this.etages.forEach(etage => {
                 let html_etage = document.createElement('div');
                 html_etage.className = "etage";
-                html_etage.title='clique moi dessus!';
-
+                html_etage.title = 'click moi dessus!';
+        
                 let img = document.createElement('img');
                 img.src = etage.image_minerai;
-                img.alt= "image à cliquer";
-
-                html_etage.appendChild(img)
-                let that = this;
-                html_etage.addEventListener("click", function() {
-                    that.click(etage)                    
-                }); 
+                img.alt = "image à clicker";
+        
                 
-                parent.appendChild(html_etage)
+                html_etage.appendChild(img);
+        
+                
+                img.addEventListener("click", () => this.click(etage));
+        
+                parent.appendChild(html_etage);
+            });
+        
+            this.checkUnlockableEtages(); 
+        }
+
+
+        checkUnlockableEtages(etage) {
+            this.etages.forEach((etage, index) => {
+                let parent = document.querySelector('#middle-section');
+                let etageDiv = parent.children[index]; 
+        
+                if (etage.est_achete) {
+                    etageDiv.style.cursor = 'pointer';
+                    etageDiv.style.pointerEvents = 'auto'; 
+                    etageDiv.style.opacity = '1';  
+                } else if (this.score >= etage.prix_etage) {
+        
+                    let img_cadena = etageDiv.querySelector('img[src="images/lock.png"]');
+                    if (img_cadena) etageDiv.removeChild(img_cadena);
+        
+                    let acheterButton = etageDiv.querySelector('.acheter-button');
+                    if (!acheterButton) {
+                        acheterButton = document.createElement('button');
+                        acheterButton.className = 'acheter-button';
+                        acheterButton.textContent = `Acheter ${etage.nom} pour ${etage.prix_etage}`;
+                        etageDiv.appendChild(acheterButton);
+        
+                        acheterButton.addEventListener('click', () => {
+                            etage.est_achete = true
+                            this.score = this.score - etage.prix_etage
+                            this.displayEtages()
+                            this.update_score()
+                        });
+                    }
+        
+                    etageDiv.style.cursor = 'not-allowed';
+                    etageDiv.style.pointerEvents = 'none'; 
+                    etageDiv.style.opacity = '0.5'; 
+                } else {
+                    etageDiv.style.cursor = 'not-allowed';
+                    etageDiv.style.pointerEvents = 'none';
+                    etageDiv.style.opacity = '0.5';
+        
+                    let img_cadena = etageDiv.querySelector('img[src="images/lock.png"]');
+                    if (!img_cadena) {
+                        img_cadena = document.createElement('img');
+                        img_cadena.src = "images/lock.png";
+                        etageDiv.appendChild(img_cadena);
+                    }
+                }
             });
         }
+    
+       
+
         update_dom(element, message){
             element.textContent = message; 
         
@@ -60,42 +112,44 @@
             let score = document.getElementById("score");
             this.update_dom(score, "Score : " + this.score);
         }
-        update_acheter_bonus_clique_button() {
-            let acheter_bonus_cliqueButton = document.getElementById("acheter_bonus_clique");
-            this.update_dom(acheter_bonus_cliqueButton, "Acheter le multiplicateur : " + this.prix_acheter_bonus_clique); 
+        update_acheter_bonus_click_button() {
+            let acheter_bonus_clickButton = document.getElementById("acheter_bonus_click");
+            this.update_dom(acheter_bonus_clickButton, "Acheter le multiplicateur : " + this.prix_acheter_bonus_click); 
         }
         update_autoclicker_button() {
             let autoclickerButton = document.getElementById("autoclicker");
             this.update_dom(autoclickerButton, "Acheter l'autoclicker : " + this.prix_autoclicker); 
         }
-        click(toto){
+        
+        click(etage, toto){
             console.log(this)
             console.log(toto)
-            this.score += this.one_click
-            this.total_score += this.one_click
+            this.score += etage.nb_click
+            this.total_score += etage.nb_click
             this.update_score()
+            this.checkUnlockableEtages(); 
         }
-        bonus_clique(){
+        bonus_click(){
             this.one_click += 1
         }
-        acheter_bonus_clique(){ // Achat
-            if (this.score >= this.prix_acheter_bonus_clique) {
-                this.score -= this.prix_acheter_bonus_clique
-                this.prix_acheter_bonus_clique = Math.ceil(this.prix_acheter_bonus_clique * 1.15)
-                this.bonus_clique()
+        acheter_bonus_click(){ // Achat
+            if (this.score >= this.prix_acheter_bonus_click) {
+                this.score -= this.prix_acheter_bonus_click
+                this.prix_acheter_bonus_click = Math.ceil(this.prix_acheter_bonus_click * 1.15)
+                this.bonus_click()
 
-                this.nb_bonus_clique += 1
-                console.log(this.nb_bonus_clique)
+                this.nb_bonus_click += 1
+                console.log(this.nb_bonus_click)
                 this.update_score()
-                this.update_acheter_bonus_clique_button()
+                this.update_acheter_bonus_click_button()
             }
         }
-        timer(){
+        timer(etage){
             setInterval(() => {
-                this.score += 1
+                this.score += etage.nb_autoclick
                 score.textContent = "Score : " + this.score;
 
-                console.lgameog(this.score)
+                console.log(this.score)
             }, 1000);
         }
         autoclicker(){ // Achat
@@ -115,7 +169,7 @@
 
 
     const game = new Game();
-    clique = document.getElementById("clique")
+    click = document.getElementById("click")
     score = document.getElementById("score")
     autoclicker = document.getElementById("autoclicker")
 
@@ -125,17 +179,14 @@
 
     
    
-    let acheter_bonus_clique = document.getElementById("acheter_bonus_clique")
-    acheter_bonus_clique.addEventListener("click", function() {
-        game.acheter_bonus_clique()  
+    let acheter_bonus_click = document.getElementById("acheter_bonus_click")
+    acheter_bonus_click.addEventListener("click", function() {
+        game.acheter_bonus_click()  
     });
 
     autoclicker.addEventListener("click", function() {
         game.autoclicker()
     });
-    
-   
-
 
 
 
